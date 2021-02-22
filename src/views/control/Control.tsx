c71e9aa8510bc2ba82e6e2b61644f79c1c5daae1
Jasper Concepcion <onetruemanok@gmail.com>
2021-02-22 10:48:21 +0800

    feat(Control): add CasterSelection

--- src/views/control/Control.tsx
+++ src/views/control/Control.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import ControlLowerThirds from "./ControlLowerThirds";
+import CasterSelection from "./CasterSelection";
 
 const Control = () => {
   return (
     <div>
       <Grid container spacing={6}>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={4}>
           <ControlLowerThirds />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <CasterSelection />
         </Grid>
       </Grid>
     </div>
