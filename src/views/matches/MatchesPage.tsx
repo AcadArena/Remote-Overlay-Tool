feda72d11486bdbc58d5401858e2d3ed4635623d
Jasper Concepcion <onetruemanok@gmail.com>
2021-02-28 13:01:13 +0800

    chore

--- src/views/matches/MatchesPage.tsx
+++ src/views/matches/MatchesPage.tsx
@@ -7,7 +7,7 @@ const MatchesPage = () => {
   return (
     <div>
       <Grid container spacing={6}>
-        <Grid item sm={12} md={6}>
+        <Grid item sm={12} md={12} lg={8}>
           <MatchesSelection />
         </Grid>
       </Grid>
