d9687d9f5aa71f2de3d67c21a7482ab6a99d547d
Jasper Concepcion <onetruemanok@gmail.com>
2021-02-28 12:59:10 +0800

    feat(ctrlLT): add featured player selection

--- src/views/control/ControlLowerThirds.tsx
+++ src/views/control/ControlLowerThirds.tsx
@@ -8,8 +8,22 @@ import SheetHeadTitle from "../../comps/sheet/SheetHeadTitle";
 import SheetBody from "../../comps/sheet/SheetBody";
 import SheetFooter from "../../comps/sheet/SheetFooter";
 import TextField from "../../comps/textfield/TextField";
-import { Typography, Grid, Button, makeStyles } from "@material-ui/core";
-import { wsContext } from "../../config/websocket/WebsocketProvider";
+import {
+  Typography,
+  Grid,
+  Button,
+  makeStyles,
+  ListSubheader,
+  MenuItem,
+  Avatar,
+  ListItemAvatar,
+  ListItemText,
+  Menu,
+} from "@material-ui/core";
+import {
+  WebsocketProps,
+  wsContext,
+} from "../../config/websocket/WebsocketProvider";
 import SheetSection from "../../comps/sheet/SheetSection";
 // import { useDocumentData } from "react-firebase-hooks/firestore";
 // import { projectFirestore as db } from "../../config/firebase/config";
@@ -25,6 +39,7 @@ import RadioButtonContainer from "../../comps/radiobutton/RadioButtonContainer";
 import {
   LowerThirds,
   LowerThirdsMode,
+  Participant,
   ReduxState,
 } from "../../config/types/types";
 
@@ -35,10 +50,84 @@ const ms = makeStyles((theme) => ({
     // minHeight: "auto!important",
     // margin: 0 + "!important",
   },
+  select: {
+    maxHeight: 300,
+    // width: 200,
+  },
+  listsubheader: {
+    backgroundColor: "#9c27b0",
+    color: "#fff",
+  },
+  featuredPlayer: {
+    display: "flex",
+    flexDirection: "column",
+
+    "& .selected": {
+      display: "flex",
+      alignItems: "center",
+      "& .teamLogo, .playerPhoto": {
+        height: 100,
+        width: 100,
+        borderRadius: 3,
+        border: "1px solid rgba(0,0,0,.1)",
+        backgroundSize: "contain",
+        // backgroundPosition: "bottom center",
+        backgroundRepeat: "no-repeat",
+        marginRight: theme.spacing(2),
+      },
+
+      "& .teamLogo": { backgroundPosition: "center" },
+      "& .playerPhoto": { backgroundPosition: "bottom center" },
+    },
+  },
 }));
+
+const SelectionItems = (
+  participants: Participant[],
+  form: LowerThirds,
+  ws: WebsocketProps,
+  onClose: () => void
+) => {
+  const classes = ms();
+  let list: any[] = [];
+  participants
+    .filter((team) => Boolean(team?.players?.length))
+    .forEach((team) => {
+      list = [
+        ...list,
+        <ListSubheader key={team.id} className={classes.listsubheader}>
+          {team.display_name}
+        </ListSubheader>,
+      ];
+
+      team.players?.forEach((p) => {
+        list = [
+          ...list,
+          <MenuItem
+            button
+            dense
+            key={p.ign}
+            onClick={() => {
+              ws.setLiveSettings({
+                lowerThirds: { ...form, player: { ...p, team } },
+              });
+              onClose();
+            }}
+          >
+            <ListItemAvatar>
+              <Avatar src={p.photo_main} />
+            </ListItemAvatar>
+            <ListItemText>{p.ign}</ListItemText>
+          </MenuItem>,
+        ];
+      });
+    });
+  return list;
+};
 
 const ControlLowerThirds = () => {
   const classes = ms();
+  const [anchorEl, setAnchorEl] = React.useState<any>(null);
   const [form, setForm] = React.useState<LowerThirds>({
     headline: "",
     ticker: "",
@@ -46,11 +135,27 @@ const ControlLowerThirds = () => {
     announcement_content: "",
     mode: "ticker",
     live: false,
+    player: undefined,
+    player_stats: {
+      left: {
+        property: "Header 1",
+        value: "Stat 1",
+      },
+      middle: {
+        property: "Header 2",
+        value: "Stat 2",
+      },
+      right: {
+        property: "Header 3",
+        value: "Stat 3",
+      },
+    },
+    player_quote: "",
   });
   const ws = React.useContext(wsContext);
 
   const live = useSelector((state: ReduxState) => state.live);
-  const { lowerThirds } = live;
+  const { lowerThirds, tournament } = live;
 
   React.useEffect(() => {
     if (!lowerThirds) return;
@@ -104,6 +209,18 @@ const ControlLowerThirds = () => {
               checked={form.mode === "long"}
               label="Long Headline"
               onClick={selectMode("long")}
+            />
+            <RadioButton
+              checked={form.mode === "playerStats"}
+              label="Player Stats"
+              disabled={!Boolean(form.player)}
+              onClick={selectMode("playerStats")}
+            />
+            <RadioButton
+              checked={form.mode === "playerQuote"}
+              label="Player Quote"
+              disabled={!Boolean(form.player)}
+              onClick={selectMode("playerQuote")}
             />
           </RadioButtonContainer>
         </SheetSection>
@@ -199,10 +316,178 @@ const ControlLowerThirds = () => {
             <AccordionDetails>
               <Grid container spacing={2}>
                 {/* Headline */}
-                <Grid item xs={12}></Grid>
+                <Grid item xs={12}>
+                  <Button
+                    onClick={({ currentTarget }) => setAnchorEl(currentTarget)}
+                    variant="contained"
+                    color="primary"
+                  >
+                    Select Player
+                  </Button>
+                  <Menu
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={() => setAnchorEl(null)}
+                    classes={{ paper: classes.select }}
+                  >
+                    {SelectionItems(
+                      tournament?.participants ?? [],
+                      form,
+                      ws,
+                      () => setAnchorEl(null)
+                    ).map((item: any) => item)}
+                  </Menu>
+                </Grid>
 
-                {/* Ticker */}
-                <Grid item xs={12}></Grid>
+                <Grid item xs={12} className={classes.featuredPlayer}>
+                  <div className="selected">
+                    <div
+                      className="teamLogo"
+                      style={{
+                        backgroundImage: `url(${form.player?.team?.logo})`,
+                      }}
+                    ></div>
+                    <div
+                      className="playerPhoto"
+                      style={{
+                        backgroundImage: `url(${form.player?.photo_main})`,
+                      }}
+                    ></div>
+                    <div className="text">
+                      <Typography className="ign">
+                        {form.player?.ign}
+                      </Typography>
+                      <Typography className="name">
+                        {form.player?.name}
+                      </Typography>
+                    </div>
+                  </div>
+                </Grid>
+
+                {/* Stats and Quote */}
+                <Grid item xs={12} className={classes.featuredPlayer}>
+                  <TextField
+                    value={form.player_quote}
+                    name="player_quote"
+                    onChange={handleChange}
+                    label="Quote"
+                    size="small"
+                  />
+                </Grid>
+                <Grid item xs={6} className={classes.featuredPlayer}>
+                  <TextField
+                    value={form.player_stats?.left?.property}
+                    name="property"
+                    onChange={({ currentTarget: { name, value } }) =>
+                      setForm({
+                        ...form,
+                        player_stats: {
+                          ...form.player_stats,
+                          left: { ...form.player_stats?.left, [name]: value },
+                        },
+                      })
+                    }
+                    label="Heading 1"
+                    size="small"
+                  />
+                </Grid>
+                <Grid item xs={6} className={classes.featuredPlayer}>
+                  <TextField
+                    value={form.player_stats?.left?.value}
+                    name="value"
+                    onChange={({ currentTarget: { name, value } }) =>
+                      setForm({
+                        ...form,
+                        player_stats: {
+                          ...form.player_stats,
+                          left: { ...form.player_stats?.left, [name]: value },
+                        },
+                      })
+                    }
+                    label="Stat 1"
+                    size="small"
+                  />
+                </Grid>
+                <Grid item xs={6} className={classes.featuredPlayer}>
+                  <TextField
+                    value={form.player_stats?.middle?.property}
+                    name="property"
+                    onChange={({ currentTarget: { name, value } }) =>
+                      setForm({
+                        ...form,
+                        player_stats: {
+                          ...form.player_stats,
+                          middle: {
+                            ...form.player_stats?.middle,
+                            [name]: value,
+                          },
+                        },
+                      })
+                    }
+                    label="Heading 1"
+                    size="small"
+                  />
+                </Grid>
+                <Grid item xs={6} className={classes.featuredPlayer}>
+                  <TextField
+                    value={form.player_stats?.middle?.value}
+                    name="value"
+                    onChange={({ currentTarget: { name, value } }) =>
+                      setForm({
+                        ...form,
+                        player_stats: {
+                          ...form.player_stats,
+                          middle: {
+                            ...form.player_stats?.middle,
+                            [name]: value,
+                          },
+                        },
+                      })
+                    }
+                    label="Stat 1"
+                    size="small"
+                  />
+                </Grid>
+                <Grid item xs={6} className={classes.featuredPlayer}>
+                  <TextField
+                    value={form.player_stats?.right?.property}
+                    name="property"
+                    onChange={({ currentTarget: { name, value } }) =>
+                      setForm({
+                        ...form,
+                        player_stats: {
+                          ...form.player_stats,
+                          right: {
+                            ...form.player_stats?.right,
+                            [name]: value,
+                          },
+                        },
+                      })
+                    }
+                    label="Heading 1"
+                    size="small"
+                  />
+                </Grid>
+                <Grid item xs={6} className={classes.featuredPlayer}>
+                  <TextField
+                    value={form.player_stats?.right?.value}
+                    name="value"
+                    onChange={({ currentTarget: { name, value } }) =>
+                      setForm({
+                        ...form,
+                        player_stats: {
+                          ...form.player_stats,
+                          right: {
+                            ...form.player_stats?.right,
+                            [name]: value,
+                          },
+                        },
+                      })
+                    }
+                    label="Stat 1"
+                    size="small"
+                  />
+                </Grid>
               </Grid>
             </AccordionDetails>
           </Accordion>
@@ -216,7 +501,9 @@ const ControlLowerThirds = () => {
             form.headline === lowerThirds?.headline &&
             form.ticker === lowerThirds?.ticker &&
             form.announcement_headline === lowerThirds?.announcement_headline &&
-            form.announcement_content === lowerThirds?.announcement_content
+            form.announcement_content === lowerThirds?.announcement_content &&
+            form.player_stats === lowerThirds?.player_stats &&
+            form.player_quote === lowerThirds?.player_quote
           }
           onClick={apply}
           color="primary"
