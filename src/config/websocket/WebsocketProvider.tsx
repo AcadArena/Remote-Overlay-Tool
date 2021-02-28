237915f5d6f3ddfff1e665d7f9b28cadf8d6636e
Jasper Concepcion <onetruemanok@gmail.com>
2021-02-28 12:58:06 +0800

    fix(WS.provider): only do 1 instance of socket.io
    there was a bug that creates multiple instances of socket.io-client

--- src/config/websocket/WebsocketProvider.tsx
+++ src/config/websocket/WebsocketProvider.tsx
@@ -1,15 +1,23 @@
 import React, { createContext } from "react";
-import { useDispatch } from "react-redux";
-import { setLiveSettings } from "../redux/Actions";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setLiveSettings,
+  setWebsocketState,
+  setWebsocketUsers,
+} from "../redux/Actions";
 import io from "socket.io-client";
+import { ReduxState, WebsocketUser } from "../types/types";
+import { store } from "../..";
 
 export interface WebsocketProps {
-  socket?: SocketIOClientStatic;
+  socket?: any;
   setLiveSettings: (settings: any) => void;
+  joinRoom: (room: string, user?: string) => void;
 }
 
 export const wsContext = createContext<WebsocketProps>({
   setLiveSettings: () => {},
+  joinRoom: () => {},
 });
 let host: string = "";
 const hostCloud: string = "https://rot-websocket-server.herokuapp.com/";
@@ -21,20 +29,40 @@ if (window.location.hostname === "localhost") {
   host = hostCloud;
 }
 
-const WebsocketProvider: React.FC = ({ children }) => {
-  let socket: any;
-  let ws: WebsocketProps = { setLiveSettings: () => {} };
-  const dispatch = useDispatch();
+let socket: any = io
+  .connect(`${host}`)
+  .on("connect", () => {
+    store.dispatch(setWebsocketState(true));
+  })
+  .on("set_live_settings", (settings: any) =>
+    store.dispatch(setLiveSettings(settings))
+  )
+  .on("usersUpdate", (users: WebsocketUser[]) =>
+    store.dispatch(setWebsocketUsers(users))
+  )
+  .on("disconnect", () => {
+    store.dispatch(setWebsocketState(false));
+  })
+  .on("connection_error", () => {
+    store.dispatch(setWebsocketState(false));
+  });
 
-  // prettier-ignore
-  if (!socket) {
-    socket = io.connect(`${host}`);
-    socket.on("set_live_settings", (settings: any) => dispatch(setLiveSettings(settings)));
-    ws = {
-      socket,
-      setLiveSettings: (settings:any) => socket.emit("set_live_settings", settings)
-    }
-  }
+const WebsocketProvider: React.FC = ({ children }) => {
+  const dispatch = useDispatch();
+  let ws: WebsocketProps = {
+    setLiveSettings: () => {},
+    joinRoom: () => {},
+  };
+  const { room: ClientRoom } = useSelector((state: ReduxState) => state.live);
+  ws = {
+    socket,
+    setLiveSettings: (settings: any) =>
+      socket.emit("set_live_settings", { data: settings, room: ClientRoom }),
+    joinRoom: (room, user?) => {
+      dispatch(setLiveSettings({ room }));
+      socket.emit("join_room", { room, username: user ?? "controller" });
+    },
+  };
   return <wsContext.Provider value={ws}>{children}</wsContext.Provider>;
 };
 
