47592ef6def0f4d8c8adbb67dd765a23b57c9990
Jasper Concepcion <onetruemanok@gmail.com>
2021-02-28 12:47:20 +0800

    feat(Actions&Reducers): add ws state and users

--- src/config/redux/Actions.ts
+++ src/config/redux/Actions.ts
@@ -1,7 +1,6 @@
-interface Payload {
-  [key: string]: any;
-}
+import { WebsocketUser } from "../types/types";
 
+type Payload = any | boolean;
 export interface Action {
   type: string;
   payload: Payload;
@@ -9,9 +8,21 @@ export interface Action {
 
 export const types = {
   LIVE_SET_SETTINGS: "setLiveSettings",
+  SET_WEBSOCKET_USERS: "setWebsocketUsers",
+  SET_WEBSOCKET_STATE: "setWebsocketState",
 };
 
 export const setLiveSettings = (settings: any): Action => ({
   type: types.LIVE_SET_SETTINGS,
   payload: settings,
+});
+
+export const setWebsocketUsers = (users: WebsocketUser[]) => ({
+  type: types.SET_WEBSOCKET_USERS,
+  payload: users,
+});
+
+export const setWebsocketState = (state: boolean) => ({
+  type: types.SET_WEBSOCKET_STATE,
+  payload: state,
 });
